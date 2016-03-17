f092e1bea55c3782bf484f74c2a487c65020d0ce
Nathaniel Kelso <nvkelso@gmail.com>
2016-03-17 16:44:47 -0700

    restore missing tile2{lat,long} functionality

--- main.js
+++ main.js
@@ -11,6 +11,14 @@ map = (function () {
 
     /*** URL parsing ***/
 
+    // convert tile coordinates to lat-lng
+    // from http://gis.stackexchange.com/questions/17278/calculate-lat-lon-bounds-for-individual-tile-generated-from-gdal2tiles
+    function tile2long(x,z) { return (x/Math.pow(2,z)*360-180); }
+    function tile2lat(y,z) {
+        var n=Math.PI-2*Math.PI*y/Math.pow(2,z);
+        return (180/Math.PI*Math.atan(0.5*(Math.exp(n)-Math.exp(-n))));
+    }
+
     // leaflet-style URL hash pattern:
     // #[zoom],[lat],[lng]
     var url_hash = window.location.hash.slice(1).split('/');
@@ -143,6 +151,8 @@ map = (function () {
 
     function long2tile(lon,zoom) { return (Math.floor((lon+180)/360*Math.pow(2,zoom))); }
     function lat2tile(lat,zoom)  { return (Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom))); }    
+
+
 
     function mapzenTileURL() {
         // find minimum max_zoom of all sources
